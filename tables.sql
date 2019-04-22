b53debd90a72ca4a424f1071d607bc1a4e008eef
basignijanat <47508727+basignijanat@users.noreply.github.com>
2019-04-22 20:31:11 +0300

    v01

---
+++ tables.sql
@@ -0,0 +1,8 @@
+CREATE TABLE userdata(
+	id int NOT NULL,
+	teacher_id int NOT NULL,
+	email varchar(255) NOT NULL,
+	password varchar(255) NOT NULL,
+	userpic varchar(255) NOT NULL,
+   PRIMARY KEY (id)
+);
