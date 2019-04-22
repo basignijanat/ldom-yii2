23838c1cf3d5d79ef7af0a7555b1db54283c1df1
basignijanat <47508727+basignijanat@users.noreply.github.com>
2019-04-22 09:07:09 +0300

    v01

--- views/site/index.php
+++ views/site/index.php
@@ -7,11 +7,9 @@ $this->title = 'My Yii Application';
 <div class="site-index">
 
     <div class="jumbotron">
-        <h1>Congratulations!</h1>
-
-        <p class="lead">You have successfully created your Yii-powered application.</p>
-
-        <p><a class="btn btn-lg btn-success" href="http://www.yiiframework.com">Get started with Yii</a></p>
+        <ul>
+			<li style="display: block;"><a href="/admin"><h2><?php echo Yii::t('app\admin', 'Administrator') ?></h2></a>			
+		</ul>
     </div>
 
     <div class="body-content">
